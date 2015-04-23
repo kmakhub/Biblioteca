a6a1c2b4126d5563d47ba816ec3a399a41a6652e
Your Name <kulanimakhubele@Kulanis-MacBook-Pro.local>
2015-04-23 16:21:51 +0200

    welcome message test implement

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,13 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private final String message = "Welcome to Biblioteca Application";;
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+    public void printWelcomeMessage() {
+        System.out.println(message);
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,7 +1,43 @@
 package com.twu.biblioteca;
 
-/**
- * Created by kulanimakhubele on 15/04/23.
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
 public class BibliotecaAppTest {
+    private  final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private  final ByteArrayInputStream inputStream = new ByteArrayInputStream("1".getBytes());
+
+
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+        System.setIn(inputStream);
+
+
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessage(){
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        String message = "Welcome to Biblioteca Application";
+        bibliotecaApp.printWelcomeMessage();
+
+        assertEquals(message + "\n", outputStream.toString());
+
+    }
 }
